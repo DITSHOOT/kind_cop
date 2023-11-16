1c135abaef1945c48c7f887c3deb6a42e23d7764
DITSHOOT <dimitri.ch42@gmail.com>
2023-11-16 22:49:04 +0100

    first-commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    prefix: "kind!",
+   // token: "MTE2MjgwNjYzNTc0MDI3MDY2Mg.G68IEd.OuZRGPKNKRDG8C7inD-xzgOWojQyurMN93j_mM"
+}
---
+++ index.js
@@ -0,0 +1,91 @@
+const Discord  = require("discord.js")
+const bot = new Discord.Client({intents: 3276799})
+const config = require('./config')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+bot.login(process.env.TOKEN)
+
+
+app.listen(PORT, () => {
+  console.log(`Le bot est en cours de lancement sur le port ${PORT}`);
+});
+
+
+let status = [
+  {
+    name: 'Patrick Sébastien',
+    type: Discord.ActivityType.Listening,
+  },
+  {
+    name: 'Netflix | Chill',
+    type: Discord.ActivityType.Watching,
+  },
+  {
+    name: 'KCX 3 | GOAT ULTIME',
+    type: Discord.ActivityType.Competing,
+  },
+
+  
+]
+
+
+bot.on('ready', () => {
+  const channel = bot.channels.cache.get('1163902591768477776');
+  if (!channel) return console.error('Le salon spécifié est introuvable.');
+  channel.send('Le bot est maintenant connecté ! <@510818650307952640>');
+    console.log(`Connecté en tant que ${bot.user.tag}`);
+
+
+
+  setInterval(() =>{
+    let random = Math.floor(Math.random() * status.length);
+    bot.user.setActivity(status[random]);
+  },3600000);
+});
+
+
+
+
+
+bot.on('messageCreate', (message) => {
+  if (message.author.bot) return; // Ne répondez pas aux messages des bots
+  if (!message.content.startsWith(config.prefix)) return; // Vérifiez s'il commence par le préfixe
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'say') {
+    // Vérifiez si l'utilisateur est un administrateur
+    if (!message.member.permissions.has('ADMINISTRATOR')) {
+      return message.reply('Seuls les administrateurs sont autorisés à utiliser cette commande.');
+    } 
+
+    // Récupère le message de l'utilisateur, en excluant le préfixe
+    const userMessage = args.join(' ');
+    if (!userMessage) {
+      return message.channel.send('Veuillez écrire un message.');
+    }
+
+    // Supprime la commande de l'utilisateur
+    message.delete();
+  
+    // Envoie le message personnalisé de l'utilisateur
+    message.channel.send(userMessage);
+  }
+});
+
+
+bot.on('messageCreate', (message) => {
+  if (message.content === '!ping') {
+    const embed = new Discord.EmbedBuilder()
+      .setTitle('Ping')
+      .setDescription('Pong!')
+      .setColor('#0099ff');
+
+    message.channel.send({ embeds: [embed] });
+  }
+
+    
+});
