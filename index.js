bffb8be147730bca5fed733ce8be6d1ce40c67bf
DITSHOOT <54135074+DITSHOOT@users.noreply.github.com>
2024-02-09 14:50:37 +0100

    addsayadmin

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ bot.on('ready', () => {
 
 
 
+
 bot.on('messageCreate', (message) => {
   if (message.author.bot) return; // Ne répondez pas aux messages des bots
   if (!message.content.startsWith(config.prefix)) return; // Vérifiez s'il commence par le préfixe
@@ -46,15 +47,20 @@ bot.on('messageCreate', (message) => {
       return message.reply('Seuls les administrateurs sont autorisés à utiliser cette commande.');
     } 
 
+    // Supprime la commande de l'utilisateur
+    message.delete();
+
+    // Vérifiez si l'utilisateur est également un administrateur pour utiliser args.join
+    if (!message.member.permissions.has('ADMINISTRATOR')) {
+      return message.reply('Seuls les administrateurs sont autorisés à utiliser cette partie de la commande.');
+    }
+
     // Récupère le message de l'utilisateur, en excluant le préfixe
     const userMessage = args.join(' ');
     if (!userMessage) {
       return message.channel.send('Veuillez écrire un message.');
     }
 
-    // Supprime la commande de l'utilisateur
-    message.delete();
-  
     // Envoie le message personnalisé de l'utilisateur
     message.channel.send(userMessage);
   }
